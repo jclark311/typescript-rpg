6e6d11baa436ebd100698432336dc868ed4a7d09
jclark311 <jon.c.clark11@gmail.com>
2026-02-20 12:02:02 -0500

    change sprite direction in animation

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,20 @@ let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 const scaledWidth = width * scale;
 const scaledHeight = height * scale;
-const cycleLoop = [8, 9, 8, 11];
+
+
+const upLoop = [0, 1, 0, 3];
+const rightLoop = [4, 5, 4, 7];
+const downLoop = [8, 9, 8, 11];
+const leftLoop = [12, 13, 12, 15];
+
+// Hero facing down
+const cycleLoop = [upLoop, rightLoop, downLoop, leftLoop];
+
 let currentLoopIndex = 0;
 // Slow down the animation
 let frameCount = 0;
+let currentDirection = 0;
 
 function drawFrame(frameX: number, frameY: number, canvasX: number, canvasY: number) {
     ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
@@ -31,10 +41,14 @@ function step() {
     frameCount = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // Draw
-    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+    drawFrame(cycleLoop[currentDirection][currentLoopIndex], 0, 0, 0);
     currentLoopIndex++;
     if (currentLoopIndex >= cycleLoop.length) {
         currentLoopIndex = 0;
+        currentDirection++;
+    }
+    if (currentDirection >= 4) {
+        currentDirection = 0;
     }
     window.requestAnimationFrame(step);
 }
