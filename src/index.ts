2a992071b77352caca0ce544aac49bb38fbf6424
jclark311 <jon.c.clark11@gmail.com>
2026-02-20 10:19:08 -0500

    add basic sprite and draw

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+let img = new Image();
+img.src = "assets/images/walk_cycle.png";
+img.onload = function() {
+    init();
+}
 
+let canvas = document.getElementById("game") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+function init() {
+    // 
+    ctx.drawImage(img, 0, 0, 16, 24, 0, 0, 16, 24);
+}
