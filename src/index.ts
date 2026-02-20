99e27853ea2c947434c6c535e7e62cc8c883fe99
jclark311 <jon.c.clark11@gmail.com>
2026-02-20 12:24:06 -0500

    add keylistener for animation

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const { scale, width, height } = constants;
 let img = new Image();
 img.src = "assets/images/walk_cycle.png";
 img.onload = function() {
-    init();
+    window.requestAnimationFrame(gameLoop);
 }
 
 let canvas = document.getElementById("game") as HTMLCanvasElement;
@@ -25,10 +25,63 @@ const cycleLoop = [upLoop, rightLoop, downLoop, leftLoop];
 let currentLoopIndex = 0;
 // Slow down the animation
 let frameCount = 0;
-let currentDirection = 0;
+const FACING_UP = 0;
+const FACING_RIGHT = 4;
+const FACING_DOWN = 8;
+const FACING_LEFT = 12;
+
+let currentDirection = FACING_DOWN;
+let keyPresses = {
+    'ArrowUp': false,
+    'ArrowRight': false,
+    'ArrowDown': false,
+    'ArrowLeft': false,
+    'w': false,
+    'a': false,
+    's': false,
+    'd': false,
+} as Record<string, boolean>;
+const MOVEMENT_SPEED = 1;
+let positionX = 0;
+let positionY = 0;
+
+window.addEventListener("keydown", keyDownListener, false);
+function keyDownListener(e: KeyboardEvent) {
+    console.log(e);
+    keyPresses[e.key] = true;
+}
+
+window.addEventListener("keyup", keyUpListener, false);
+function keyUpListener(e: KeyboardEvent) {
+    console.log(e.key);
+    keyPresses[e.key] = false;
+}
 
 function drawFrame(frameX: number, frameY: number, canvasX: number, canvasY: number) {
     ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (keyPresses.w) {
+        positionY -= MOVEMENT_SPEED;
+        currentDirection = FACING_UP;
+    } else if (keyPresses.s) {
+        positionY += MOVEMENT_SPEED;
+        currentDirection = FACING_DOWN;
+    }
+    if (keyPresses.a) {
+        positionX -= MOVEMENT_SPEED;
+        currentDirection = FACING_LEFT;
+    } else if (keyPresses.d) {
+        positionX += MOVEMENT_SPEED;
+        currentDirection = FACING_RIGHT;
+    }
+
+    drawFrame(currentDirection, 0, positionX, positionY);
+
+    window.requestAnimationFrame(gameLoop);
 }
 
 
